3df2103ad93742a73e2c0825b2a773ca21e5b5ad
remiPra <remipradere@gmail.com>
2025-03-11 23:15:52 +0100

    whatsapp button system

--- src/data/robes.js
+++ src/data/robes.js
@@ -1,13 +1,13 @@
 import boheme from "./style-boheme-chic-dynamique.json";
 import princesse from "./forme-princesse-dynamique.json";
 import minimaliste from "./minimaliste-dynamique.json";
-import sirene from "./forme-sirene-dynamique.json";
+import sireneRobes from "./forme-sirene-dynamique.json"; // <-- RENOMMAGE
 
 // Sélectionner uniquement les 4 premières robes du style "princesse"
 const princessRobes = princesse.slice(0, 4);
 
 // Fusionner toutes les robes et enlever les doublons
-const allRobes = [...princessRobes, ...sirene, ...minimaliste, ...boheme];
+const allRobes = [...princessRobes, ...sireneRobes, ...minimaliste, ...boheme];
 const robes = allRobes.reduce((acc, robe) => {
   if (!acc.some((r) => r.dressName === robe.dressName)) {
     acc.push(robe);
