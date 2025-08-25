c230f1dda682a1401090517cb497f63148ffc994
remiPra <remipradere@gmail.com>
2025-08-25 15:47:56 +0200

    deepseek en place man

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -1,6 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log('✅ SW enregistré !'))
-      .catch(err => console.error('❌ SW erreur :', err));
-  }
-  
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
